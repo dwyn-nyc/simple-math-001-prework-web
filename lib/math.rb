a654b626faea2bc2b2a1b6efadd8c6e20c21d33b
dwyn-nyc <dwyn.nyc@gmail.com>
2016-01-25 15:40:39 -0500

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,34 @@
+num = 37460982349
+num1 = 98746022358
+num2 = 7294296280
+num3 = 5490834688796
+num4 = 34876390846
+
+
 def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
+  num1 - num2
 end
 
 def division(num1, num2)
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 + (( num2 * num3 ) / num4 )
 end
